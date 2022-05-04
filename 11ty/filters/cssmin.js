210b8795b2ec78ec024079f3a30b5fbf101740a5
Bradley Burgess <bradleyburgess@gmail.com>
2022-05-04 12:00:28 +0200

    add env logic to cssmin

--- 11ty/filters/cssmin.js
+++ 11ty/filters/cssmin.js
@@ -1,5 +1,14 @@
 const CleanCSS = require('clean-css');
 
 module.exports = function (code) {
-  return new CleanCSS({}).minify(code).styles;
+  if (process.env.NODE_ENV === 'production') {
+    try {
+      const css = new CleanCSS({}).minify(code).styles;
+      return css;
+    } catch (error) {
+      console.log('Error minifying css!:');
+      console.log(error);
+    }
+  }
+  return code;
 };
