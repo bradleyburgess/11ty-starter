ace00de3bd5a9e56501dd7a7020673e40fb309db
Bradley Burgess <bradleyburgess@gmail.com>
2022-05-04 10:06:08 +0200

    Added criticalCss

--- src/_data/googlefonts.js
+++ src/_data/googlefonts.js
@@ -1,12 +1,30 @@
 const EleventyFetch = require('@11ty/eleventy-fetch');
 
-module.exports = async () => {
-  const url =
-    'https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@300;400;500;600;700&display=swap';
+const fonts = [
+  'Noto+Serif:ital,wght@0,400;0,700;1,400;1,700',
+  'Open+Sans:wght@300;400;500;600;700',
+];
 
-  const css = await EleventyFetch(url, {
-    duration: '1d',
-    type: 'text',
-  });
-  return css;
+const fontString =
+  'https://fonts.googleapis.com/css2?' +
+  fonts.map((font) => 'family=' + font).join('&') +
+  '&display=swap';
+
+module.exports = async () => {
+  const url = fontString;
+
+  try {
+    const css = await EleventyFetch(url, {
+      duration: '1d',
+      type: 'text',
+      headers: {
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+      },
+    });
+    return css;
+  } catch (error) {
+    console.log('Failed to get Google Fonts');
+    throw new Error(error);
+  }
 };
--- src/_pages/index.md
+++ src/_pages/index.md
@@ -1,9 +1,11 @@
 ---
-layout: 'layouts/with-meta.njk'
+layout: 'layouts/base.njk'
 permalink: /
-og_image:
+ogImage:
   src: 'https://images.unsplash.com/photo-1566913485268-1287f67f87fe?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MTMwOTI2Nw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=3200'
   alt: 'A violin'
+criticalCss: 
+  - index
 ---
 
 # Home
